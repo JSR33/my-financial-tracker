d327363bd4c3784c7f4659c4d940fbb30c7a8328
JSR33 <joao.norbain@gmail.com>
2022-01-09 16:36:47 +0000

    API versioning

--- MyFinancialTrackerAPI/MyFinancialTrackerAPI/Startup.cs
+++ MyFinancialTrackerAPI/MyFinancialTrackerAPI/Startup.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+using MyFinancialTrackerAPI.Installers;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace MyFinancialTrackerAPI
 {
@@ -26,12 +21,16 @@ namespace MyFinancialTrackerAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var installers = typeof(Startup).Assembly.ExportedTypes
+                .Where(x => 
+                    typeof(IInstaller).IsAssignableFrom(x) && !x.IsAbstract && !x.IsInterface
+                )
+                .Select(Activator.CreateInstance)
+                .Cast<IInstaller>()
+                .ToList();
 
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyFinancialTrackerAPI", Version = "v1" });
-            });
+            installers.ForEach(installer => installer.InstallService(services, Configuration));
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
